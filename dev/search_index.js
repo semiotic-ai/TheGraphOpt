c2ff219519e229b1e2fdbc76ae1adcc80bd4c1f7
Documenter.jl <documenter@juliadocs.github.io>
2022-10-12 17:01:31 +0000

    build based on 1bf6db9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TheGraphOpt","category":"page"},{"location":"#TheGraphOpt","page":"Home","title":"TheGraphOpt","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TheGraphOpt.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TheGraphOpt]","category":"page"}]
+}
